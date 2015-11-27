8fafbb2b25550f247473b99396ec7983e50fd656
Alex Pop <al3xpop@gmail.com>
2015-11-27 15:36:49 +0100

    working with inspec, no chef installed

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,11 +1,9 @@
-# use basic tests
 describe package('siege') do
   it { should be_installed }
 end
 
 describe command('siege -V') do
-  it "executes without error" do
+  it 'executes without error' do
     expect(subject.exit_status).to eq 0
   end
 end
-
